999cbee4a76fc9c93caee387e28e19dd0b7147ef
Rizwan Shereef <34895602+rizwanshereef@users.noreply.github.com>
2018-10-29 20:53:16 +0530

    Add comments to the program

--- Greeting.py
+++ Greeting.py
@@ -1,3 +1,4 @@
+# program that compares with present time and prints greeting wishes
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -8,12 +9,12 @@ import datetime
 now = datetime.datetime.now()
 hour = now.hour
 
-if hour < 10:
+if hour < 10:  # Compare value with current hour and if it is less than 10 then its Good morning
     printf("Good morning World!")
-elif hour < 17:
+elif hour < 17: # If the value of hour is less than 17 ie 5pm then its Hello world
     printf("Hello World!")
 else:
-    printf("Good evening World")
+    printf("Good evening World") # if neither of the 2 conditions are false then its good evening
     
 
 
